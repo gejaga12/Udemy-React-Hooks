609d11f4996aa5b5573dbf9ddbdae5eb0d7e3a35
Gerardo Javier García <gejaga12@hotmail.com>
2022-08-17 09:21:39 -0300

    Update Ternario

---
+++ src/bases/12-ternario.js
@@ -0,0 +1,16 @@
+const activo = true;
+
+// let msj = "";
+
+// if (!activo) {
+//   msj = "Activo";
+// } else {
+//   msj = "Inactivo";
+// }
+
+// const msj = (activo) ? "Activo" : "Inactivo";
+// const msj = (activo) ? "Activo" : null;
+
+const msj = activo && "Activo";
+
+console.log(msj);
